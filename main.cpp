436dd45170b7527b6e976ba9ef436db64070b001
lukaszmasko2000 <lukasz.masko2000@gmail.com>
2026-08-15 15:28:12 +0200

    std::string changed to auto in 18 line

--- main.cpp
+++ main.cpp
@@ -15,7 +15,7 @@ int main(void)
 
     for (auto i{0} ; i < 7 ; ++i)
     {
-        std::string znak = generator.generujZnakWywolawczy();
+        auto znak = generator.generujZnakWywolawczy();
         auto paliwo = generator.losujRozkladNormalny(100.0, 20.0);
         if (i % 2 == 0) kontrolaLotow.dodajDoHangaru(std::make_unique<Samolot>(znak, paliwo));
         else kontrolaLotow.dodajDoHangaru(std::make_unique<Dron>(znak, paliwo));
