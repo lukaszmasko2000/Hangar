6a1218eda7261e025e977b7f4400c989aee93ce7
lukaszmasko2000 <lukasz.masko2000@gmail.com>
2026-08-15 13:39:44 +0200

    I connected main.cpp with GeneratorLotow.h and AirControl.h

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include "GeneratorLotow.h"
+#include "AirControl.h"
+
+
+#include <iostream>
+
+
+int main(void)
+{
+    std::setlocale(LC_ALL, "Polish");
+
+    GeneratorLotow& generator = GeneratorLotow::getInstance();
+    AirControl kontrolaLotow;
+
+
+    return 0;
+}
