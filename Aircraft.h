871f090f8fdce6c893261cc06a7b86aa18b6f407
lukaszmasko2000 <lukasz.masko2000@gmail.com>
2026-08-15 12:12:06 +0200

    Added Aircraft class with nested CzarnaSkrzynka class

---
+++ Aircraft.h
@@ -0,0 +1,37 @@
+#ifndef AIRCRAFT_H
+#define AIRCRAFT_H
+
+#include <string>
+#include <iostream>
+#include <list>
+
+
+
+class Aircraft
+{
+public:
+    Aircraft(const std::string& zW, double p)
+        : znakWywolawczy(zW), paliwo(p){
+            skrzynka.zapisz("Utworzono obiekt: " + znakWywolawczy);
+    }
+
+    
+
+
+protected:
+    std::string znakWywolawczy;
+    double paliwo;
+    
+    class CzarnaSkrzynka{
+    public:
+        void zapisz(const std::string& logi);
+        void wyswietl() const;
+    private:
+        std::list<std::string> logi;
+    };
+
+    CzarnaSkrzynka skrzynka;
+       
+};
+
+#endif // AIRCRAFT_H
