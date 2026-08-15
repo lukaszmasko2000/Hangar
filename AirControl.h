6d09373d21113d5f380963ebe84d229430680f44
lukaszmasko2000 <lukasz.masko2000@gmail.com>
2026-08-15 13:12:18 +0200

    Added empty AirControl class and connect it with Aircraft header

---
+++ AirControl.h
@@ -0,0 +1,19 @@
+#ifndef AIRCONTROL_H
+#define AIRCONTROL_H
+
+#include "Aircraft.h"
+#include <vector>
+#include <memory>
+#include <iostream>
+
+
+class AirControl
+{
+    
+
+};
+
+
+
+
+#endif // AIRCONTROL_H
