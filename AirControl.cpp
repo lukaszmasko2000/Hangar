93dc64f48174da97d7a83e4e4b32c5e05f7809c3
lukaszmasko2000 <lukasz.masko2000@gmail.com>
2026-08-15 13:27:28 +0200

    I added some definition in AirControl.cpp for functions

---
+++ AirControl.cpp
@@ -0,0 +1,24 @@
+#include "AirControl.h"
+
+
+
+void AirControl::dodajDoHangaru(std::unique_ptr<Aircraft> a)
+{
+    hangar.push_back(std::move(a));
+}
+
+void AirControl::przekazDoLadowania(const std::string& fragment)
+{
+    std::cout << "Szukanie jednostek z fragmentem '" << fragment << "'..." << std::endl;
+    auto it = hangar.begin();
+    while (it != hangar.end())
+    {
+        if ((*it)->getZnak().find(fragment) != std::string::npos)
+        {
+            (*it)->addLog("Podejscie do ladowania.");
+            kolejkaLotow.push_back(std::move(*it));
+            it = hangar.erase(it);
+        }
+        else ++it;
+    }
+}
