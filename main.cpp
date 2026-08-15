e1adbbdcc24bb965d028eb4832c5acd8456d9af4
lukaszmasko2000 <lukasz.masko2000@gmail.com>
2026-08-15 13:44:14 +0200

    I added some code to show how's program working

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include "GeneratorLotow.h"
 #include "AirControl.h"
-
+#include "Samolot.h"
+#include "Dron.h"
 
 #include <iostream>
 
@@ -12,6 +13,17 @@ int main(void)
     GeneratorLotow& generator = GeneratorLotow::getInstance();
     AirControl kontrolaLotow;
 
+    for (auto i{0} ; i < 7 ; ++i)
+    {
+        std::string znak = generator.generujZnakWywolawczy();
+        auto paliwo = generator.losujRozkladNormalny(100.0, 20.0);
+        if (i % 2 == 0) kontrolaLotow.dodajDoHangaru(std::make_unique<Samolot>(znak, paliwo));
+        else kontrolaLotow.dodajDoHangaru(std::make_unique<Dron>(znak, paliwo));
+    }
+
+    kontrolaLotow.przekazDoLadowania("-");
+    kontrolaLotow.symulujKolejke();
+    kontrolaLotow.wyswietlRaportyKoncowe();
 
     return 0;
 }
